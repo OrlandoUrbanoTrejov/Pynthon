7adbf95b67d064f58c15908bad907a1ef70e5555
Orlando Urbano Trejo <91504341+OrlandoUrbanoTrejov@users.noreply.github.com>
2021-11-02 11:11:00 -0600

    Add files via upload

---
+++ HolaMundo.py
@@ -0,0 +1,10 @@
+# Hola Mundo en Phyton
+# Title          :Hola Mundo en Python
+# Description    :Hola Mundo en Python desde VIM con Windows
+# Author         :Orlando Urbano Trejo (Lando)
+# Date           :20211005
+# Version        :1
+# Usage          :C:\>python /path/HolaMundo.py
+
+print ("Hola Mundo en Python desde Vim con Windows Orlando")
+
